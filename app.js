4a8afe538edd03378e57f53256212086a1032a3f
TatianaHernandez4 <tatiana.hernandezposada@gmail.com>
2024-05-02 19:35:26 -0500

    tres

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from "cors";
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+app.listen(port,() => {
+    console.log(`Example app listening on port ${port}`);
+});
