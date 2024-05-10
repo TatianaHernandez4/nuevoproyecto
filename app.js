26f5824eb00f1fdf0835eb64e6f9158655d6468f
TatianaHernandez4 <tatiana.hernandezposada@gmail.com>
2024-05-09 20:51:34 -0500

    holis

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 import productRouter from "./routes/product.route.js";
+import db from "./database/db.js";
+// import db from "./database/db.js";
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors()); 
-app.use(express.static("public"));
+// bd conection
+(async () => {
+  try {
+    await db.authenticate();
+    db.sync(); // crea las tablas en la db ( si no existen )
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.message);
+  }
+})();
 
-//http://localhost:3000/products
+// midellewares
+app.use(express.json()); // envio-recepción de información en formato tipo json
+app.use(cors()); // consumo de API desde otros puertos diferentes al PORT
+app.use(express.static("public")); // contenedor de archivos estaticos - carpeta publica
+// http://localhost:3000
+
+// endpoints
+
+// forma larga
+// http://localhost:3000/products
 // app.get("/products", (req, res) => {
-//     res.json({
-//         success: true,
-//         data:[
-//             {
-//                 subject: "programacion v",
-//                 semester: "7",
-//                 date: new Date().toDateString(),
-//             },
-//         ],
-//     });
+//   // res.send("Hello World!");
+//   res.status(200).json({
+//     success: true,
+//     data: [
+//       {
+//         subject: "programación v",
+//         description: "this is my first api",
+//         hour: "20:00pm",
+//         semester: "7",
+//         date: new Date().toDateString(),
+//       },
+//     ],
+//   });
 // });
 
-app.use("/products",productRouter);
+// forma corta
+app.use("/products", productRouter);
 
-app.listen(port,() => {
-    console.log(`Example app listening on port ${port}`);
+// levantar aplicación
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
